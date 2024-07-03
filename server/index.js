f3820cbdf7987921552790f9dbde4f7e428421a4
Zaid Nasim <zaid.btech2022@sitpune.edu.in>
2024-07-03 20:43:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://node-mailer-gxxv.vercel.app/",
+    origin: "https://node-mailer-gxxv.vercel.app",
     credentials: true,
 }));
 
